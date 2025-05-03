dea4ca9a7295689e646c34b7be6a1f3d1121e1f5
moderatorman <12193288+moderatorman@users.noreply.github.com>
2025-05-03 11:51:38 -0700

    Add login banner configuration (for location)

--- src/main/java/me/moderatorman/arpasim/impl/programs/LoginBannerProgram.java
+++ src/main/java/me/moderatorman/arpasim/impl/programs/LoginBannerProgram.java
@@ -3,12 +3,17 @@ package me.moderatorman.arpasim.impl.programs;
 import me.moderatorman.arpasim.impl.ProgramManager;
 import me.moderatorman.arpasim.util.IProgramIO;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class LoginBannerProgram extends AbstractProgram
 {
+    private static final String CONFIG_FILE = "loginbanner.properties";
+
     public LoginBannerProgram()
     {
         super("loginbanner", "Displays the login banner", "", true);
@@ -49,16 +54,32 @@ public class LoginBannerProgram extends AbstractProgram
         int userCount = 1; //TODO: track online users
 
         String banner = String.format(
-                "It is %s on %s in Mountain View, California, USA.\n" +
+                "It is %s on %s in %s.\n" +
                         "There are %s local user(s) currently connected.\n\n" +
                         "May the command line live forever.\n\n" +
                         "Command, one of the following:\n%s\n" +
                         "More commands available after login Type HELP for a detailed command list.\n" +
                         "Type NEWUSER to create an account. Press control-C to interrupt any command.",
-                time, date, userCount, commandsGrid.toString()
+                time, date, getServerLocation(), userCount, commandsGrid
         );
 
         ioHandler.println(banner);
         ioHandler.flush();
+    }
+
+    private String getServerLocation()
+    {
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream(CONFIG_FILE))
+        {
+            properties.load(input);
+            String city = properties.getProperty("city", "Mountain View");
+            String region = properties.getProperty("region", "California");
+            String country = properties.getProperty("country", "USA");
+            return String.format("%s, %s, %s", city, region, country);
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+            return "Unknown Location";
+        }
     }
 }
